fbfc5bd53ab8e4846527a5fdd78ba1a54e416f77
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-02-26 14:39:26 -0500

    q5 finished

--- hw2/q5/main.py
+++ hw2/q5/main.py
@@ -1,1 +1,14 @@
-# Your code goes here.
+from sys import stdin
+
+line = stdin.readline().split(" ")
+K = int(line[0])
+H = int(line[1])
+
+result = 0
+j = 0
+
+while result + K * 2 ** j < H:
+    result += K * 2 ** j
+    j += 1
+
+print(H - result)
--- hw2/q6/main.py
+++ hw2/q6/main.py
@@ -1,1 +1,32 @@
-# Your code goes here.
+from sys import stdin
+
+line = stdin.readline().split(" ")
+change = ""
+
+files = [1]
+for file in stdin.readline().split(" "):
+    files.append(int(file))
+    
+out_of_order = False
+i = 1
+out = ""
+while i < len(files):
+    
+    if files[i - 1] > files[i]:
+        if len(out) > 0:
+            print("no")
+            exit()
+        
+        old_i = i
+        while files[i - 1] > files[i]:
+            i += 1
+            
+        if i > old_i:
+            out = "reverse"
+        else:
+            out = "swap"     
+    
+    i += 1
+    
+print("no")
+print(out)
