800c064d478a6d0d16a2c6ecbc30f9d79eaaaca5
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2023-02-26 16:22:33 -0500

    q6 finished

--- hw2/q6/main.py
+++ hw2/q6/main.py
@@ -1,32 +1,50 @@
 from sys import stdin
 
 line = stdin.readline().split(" ")
-change = ""
 
-files = [1]
+files = [0] # 0 place holder to move up index for indexing from 1
 for file in stdin.readline().split(" "):
     files.append(int(file))
-    
-out_of_order = False
+  
 i = 1
 out = ""
-while i < len(files):
+swap_i = -1
+while i + 1 < len(files):
     
-    if files[i - 1] > files[i]:
+    if files[i] > files[i + 1]:
         if len(out) > 0:
-            print("no")
-            exit()
-        
-        old_i = i
-        while files[i - 1] > files[i]:
-            i += 1
+            if swap_i != -1:
+                out = f"swap {swap_i} {i + 1}"
+                swap_i = -1
+                i += 1
+                continue
+            else:
+                print("no")
+                exit()
             
-        if i > old_i:
-            out = "reverse"
+        if i + 2 < len(files) and files[i + 1] > files[i + 2]:
+            old_i = i
+            while i + 1 < len(files) and files[i] > files[i + 1]:
+                i += 1
+                
+            out = f"reverse {old_i} {i}"
+            
         else:
-            out = "swap"     
-    
+            out = f"swap {i} {i + 1}"
+            if len(files) > 3:
+                swap_i = i
+            
     i += 1
+                
+if swap_i == -1:
+    print("yes")
+    if len(out) > 0:
+        print(out)
+else:
+    print("no")
+            
+        
+        
+        
+
     
-print("no")
-print(out)
