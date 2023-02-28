f923f83f46742a6908273fd64b5b5380c34b7a5f
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2023-02-27 21:13:12 -0500

    Finished 7

--- hw2/q7/main.py
+++ hw2/q7/main.py
@@ -1,10 +1,24 @@
 from sys import stdin
-import statistics
+import bisect
 
-line = stdin.readline()
+def get_median():
+    if len(numbers) == 0:
+        print('Wrong!')
+        return
+    elif len(numbers) % 2 == 0: # Even
+        out = (numbers[len(numbers) // 2] + numbers[(len(numbers) // 2) - 1]) / 2
+    else: # Odd
+        out = numbers[len(numbers) // 2]
+        
+    if int(out) == out:
+        print(int(out))
+    else:
+        print(out)
 
+line = stdin.readline()  
 
-nums = []
+numbers = []
+content = {}
 
 for line in stdin.readlines():
     line = line.split(" ")
@@ -12,12 +26,20 @@ for line in stdin.readlines():
     operation = line[0]
     x = int(line[1])
     
-    
-    # Read
-    if operation == 'r':
-        pass
-    
-    # Write
+    # Add
+    if operation == 'a':
+        bisect.insort(numbers, x) # This line is not from the below source
+        if x in content:
+            content[x] += 1
+        else:
+            content[x] = 1
+        get_median()       
+         
+    # Remove
+    elif x in content and content[x] > 0:
+        del numbers[bisect.bisect_left(numbers, x)] # This line is from https://stackoverflow.com/a/17348367, it is used to delete the left most occurance of the value
+        content[x] -= 1
+        get_median()
     else:
-        pass
+        print("Wrong!")
         
