339b06efc14bc709eb08311d308e40c777c496ec
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2023-02-27 23:56:05 -0500

    q8 finished

--- hw2/q8/main.py
+++ hw2/q8/main.py
@@ -1,1 +1,21 @@
-# Your code goes here.
+from sys import stdin
+
+stdin.readline()  
+
+hives = []
+warriors = 0
+start = 0
+for line in stdin.readlines():
+     
+    line = line.split(" ")
+    hives.append((int(line[0]), int(line[1])))
+    
+    while warriors < 0:
+        warriors -= hives[start][0] - hives[start][1]
+        start += 1
+        
+    warriors += hives[-1][0] - hives[-1][1]
+
+print(start)
+        
+        
