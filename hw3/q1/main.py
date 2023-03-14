6e336b47e632d82818d33159c1a4117862b4eaac
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2023-03-14 15:40:32 -0400

    Q1 Done

--- hw3/q1/main.py
+++ hw3/q1/main.py
@@ -1,1 +1,10 @@
-# Your code goes here.
+from sys import stdin
+
+k_stabalizers = int(stdin.readline().split(" ")[1])
+costs = sorted([int(i) for i in stdin.readline().split(" ")], reverse=True)
+
+total = 0
+for i in range(len(costs)):
+    total += costs[i] * ((i // k_stabalizers) + 1)
+
+print(total)
