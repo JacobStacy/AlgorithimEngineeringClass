8cf872f5c853c0771053095509674f6eeef5795f
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-03-15 19:57:01 -0400

    q4 done

--- hw3/q4/main.py
+++ hw3/q4/main.py
@@ -1,36 +1,37 @@
 from sys import stdin
-import heapq
 
-testing = True
-
-if not testing:
-    n_tests = int(stdin.readline())
-else:
-    n_tests = 1
-
-
-for i in range(n_tests):
-    if not testing:
-        crystals = [int(stdin.readline().split(" ")[0])]
-        frequencies = sorted([int(i) for i in stdin.readline().split(" ")], reverse=True)
-    else:
-        crystals = [int("12 3".split(" ")[0])]
-        frequencies = sorted([int(i) for i in "2 3 4".split(" ")], reverse=True)
-        
-        crystals = [int("84 4".split(" ")[0])]
-        frequencies = sorted([int(i) for i in "42 7 6 3".split(" ")], reverse=True)
-        
-        crystals = [int("64 5".split(" ")[0])]
-        frequencies = sorted([int(i) for i in "2 4 8 16 64".split(" ")], reverse=True)
-    hours = 0
+n_tests = int(stdin.readline())
     
-    max_under = {}
-    
-    
-def find_max_under(freq, frequencies, max_under):
-    
+# Finds best hours under each chunk
+def find_max_under(chunk, frequencies, max_under):
     
     max = 0
+    # Loop through every frequency and find the max
     for i in range(len(frequencies)):
-        frequencies[i]
+        
+        # Skip if not a valid frequency
+        if chunk % frequencies[i] != 0 or chunk <= frequencies[i]:
+            continue
+        
+        # Check if chunks made by new frequency can be broken up further and cache it
+        if frequencies[i] not in max_under and i + 1 <= len(frequencies):
+            max_under[frequencies[i]] = find_max_under(frequencies[i], frequencies[i+1:], max_under)  
+        
+        # Check if the the frequency is better than the last
+        chunks_created = 1 + ((chunk // frequencies[i]) * max_under[frequencies[i]])
+        if chunks_created >= max: max = chunks_created
+               
+    return max
+
+for i in range(n_tests):
+    
+    crystal = int(stdin.readline().split(" ")[0])
+    frequencies = sorted([int(i) for i in stdin.readline().split(" ")], reverse=True)
+    
+    print(find_max_under(crystal, frequencies, {}))
+
+                
+    
+                    
+                    
     
