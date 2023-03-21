526deeec04051a58051282a9b5724e9785449d0b
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-03-21 18:50:10 -0400

    working on q8

--- hw3/q8/main.py
+++ hw3/q8/main.py
@@ -1,1 +1,21 @@
-# Your code goes here.
+from sys import stdin
+
+line = stdin.readline()
+n_hats, id = int(line[0]), int(line[1])
+    
+    
+ 
+
+colors = stdin.readline().split()
+seen = stdin.readline().split()
+
+numbers = {}
+for i in range(colors):
+    numbers[colors] = i
+
+
+sum = 0
+for color in seen:
+    sum += numbers[color]
+    
+print(sum % n_hats)
