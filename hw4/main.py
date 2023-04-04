e816679aa86b9ae867c25f6aff4177ac850b78fe
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-04-03 21:18:55 -0400

    hw4 final solution

--- hw4/main.py
+++ hw4/main.py
@@ -1,116 +1,75 @@
 from sys import stdin
-import math
 
-testing = True
+rarity = dict()
+have_skill = dict()
+ids = dict()
+
+previous = dict()
+
+n_people, k_skills = [int(i) for i in stdin.readline().split(" ")]
+all_skills = stdin.readline().strip().split(" ")
+required_skills = set(all_skills)
+crewmates = set()
+
+for i in range(n_people * 2):
+    if i % 2 == 0:
+        stdin.readline()
+        continue
+    
+    skills = set()
+    line = stdin.readline().strip()
+    if line in previous:
+        continue
+    previous[line] = None
+    for skill in line.split(" "):
+        
+        if skill in required_skills:
+            if skill not in rarity:
+                rarity[skill] = 1
+            else:
+                rarity[skill] += 1
+            skills.add(skill)
+            if skill not in have_skill:
+                have_skill[skill] = [len(crewmates)]
+            else:
+                have_skill[skill].append(len(crewmates))
+                
+        
+    if len(skills) > 0:
+        ids[len(crewmates)] = skills
+        crewmates.add(len(crewmates))
+            
 
 
 
-if not testing:
-    n_people, k_skills = [int(i) for i in stdin.readline().split(" ")]
-    required_skills = set(stdin.readline().strip().split(" "))
-    crewmates = []
-    for i in range(n_people * 2):
-        if i % 2 == 0:
-            a = stdin.readline()
+
+def find_min(skills_required, current_min, selected):
+    
+    min = current_min
+    
+    for crewmate in have_skill[sorted(skills_required, key=lambda x: rarity[x])[0]]:
+        
+        if crewmate in selected:
             continue
         
-        skills = set()
+        c_skills = set()
         
-        for skill in stdin.readline().strip().split(" "):
-            if skill in required_skills:
-                skills.add(skill)
-                
-        if len(skills) > 0:
-            crewmates.append(skills)
+        for c_skill in ids[crewmate]:
+            c_skills.add(c_skill)
         
-else:
-    n_people, k_skills = 15, 12
-    required_skills = set("Botany Neuroscience ArtificialLife Batteries Metals InformationTheory".split(" "))
-    crewmates = []
-    # for line in "Botany Neuroscience ArtificialLife\nBotany Batteries\nBotany\nBotany Batteries Metals InformationTheory\nNeuroscience Batteries InformationTheory".split('\n'):
-    #     skills = set()
+        skills_left = skills_required-c_skills
         
-    #     for skill in line.split(" "):
-    #         if skill in required_skills:
-    #             skills.add(skill)
-                
-    #     if len(skills) > 0:
-    #         crewmates.append(skills)
-    crewmates = [{'Medicine', 'Plastics', 'EvolutionaryBiology', 'Statistics'}, {'Cosmology', 'Plastics', 'Sensors', 'EvolutionaryBiology'}, {'Medicine', 'Cryptography', 'EvolutionaryBiology', 'Statistics'}, {'Medicine', 'Sensors', 'EvolutionaryBiology', 'Cosmology'}, {'AgentBasedModeling', 'Sensors', 'EvolutionaryBiology', 'AlienLanguages'}, {'Plastics', 'CloudComputing', 'Statistics', 'AgentBasedModeling'}, {'Medicine', 'Plastics', 'CloudComputing', 'Sensors'}, {'CloudComputing', 'Plastics', 'Polymers', 'AgentBasedModeling'}, {'Medicine', 'PersonalizedMedacine', 'Polymers', 'Statistics'}, {'Cosmology', 'Polymers', 'Statistics', 'AlienLanguages'}, {'PersonalizedMedacine', 'Cryptography', 'Statistics', 'AgentBasedModeling'}, {'Medicine', 'AgentBasedModeling', 'PersonalizedMedacine', 'AlienLanguages'}, {'PersonalizedMedacine', 'CloudComputing', 'Polymers', 'Cosmology'}, {'PersonalizedMedacine', 'Polymers', 'Cryptography', 'AlienLanguages'}, {'CloudComputing', 'Sensors', 'Cryptography', 'AlienLanguages'}]
-    required_skills = {'PersonalizedMedacine', 'CloudComputing', 'AlienLanguages', 'Sensors', 'AgentBasedModeling', 'Cosmology', 'Plastics', 'Polymers', 'Medicine', 'Cryptography', 'EvolutionaryBiology', 'Statistics'}
-    
-
-
-
-
-# def least_needed(crewmates, skills, n_included):
-    
-#     if n_included > n_people:
-#         return None
-    
-#     if len(skills) >= k_skills:
-#         return n_included
-    
-#     include = least_needed(crewmates[:-1], skills.union(skills), n_included + 1)
-#     dont = least_needed(crewmates[:-1], skills, n_included)
-    
-#     if include is None:
-#         return dont
-    
-#     if dont is None:
-#         return include
-    
-#     if include < dont:
-#         return include
-#     else:
-#         return dont
-    
-# print(least_needed(crewmates, set(), 0))
-
-def get_leading_bit(num):
-    return 2**math.floor(math.log2(num))
-    
-def get_mate(num, crewmates):
-    j = -1
-    while 1:
-        if num <= 0 or -j > len(crewmates):
-            break
+        under = 0
+        if len(skills_left) != 0:
+            under = find_min(skills_left, min, selected.union({crewmate}))
         
-        if 1 & num:
-            return crewmates[j]
+            if under is None:
+                continue
         
-        num>>=1
-        j-=1
-    
-skills_under_num = {}
-
-min = None
-for i in range(1, 2**len(crewmates) - 1):
-    j = -1
-    skills = set()
-    n_included = 1
-    
-    
-    leading_bit = get_leading_bit(i)  
-    if i == 32768:
-        pass 
-    crewmate = get_mate(leading_bit, crewmates)
-    
-    remaining = i ^ leading_bit
-    
-    if remaining:
-        skills = crewmate.union(skills_under_num[remaining][0])
-        n_included += skills_under_num[remaining][1]
-    else:
-        skills = crewmate
-    
-
-    skills_under_num[i] = (skills, n_included)
-    
-    if len(skills) >= k_skills:
-        if min is None or n_included < min:
-            min = n_included
+        if min > 1 + under:
+            min = 1 + under
             
+    return min
     
-        
-print(min)
+
+print(find_min(required_skills,n_people,set()))
