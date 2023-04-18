c658ea1a3828a359ebe578a1d951d56ed1b52230
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-04-18 16:57:49 -0400

    q2 done

--- hw5/q2/main.py
+++ hw5/q2/main.py
@@ -1,1 +1,31 @@
-# Your code goes here.
+from sys import stdin
+
+conn_map = dict()
+edges = []
+
+n_sites, m_edges = [int(j) for j in stdin.readline().split(' ')]
+lines = stdin.readlines()
+    
+for i in range(n_sites):
+    conn_map[i] = set()
+
+for i in range(m_edges):
+    first, second = [int(j) for j in lines[i].split(' ')]
+    conn_map[first].add(second)
+    conn_map[second].add(first)
+    edges.append((first,second))
+
+max = 0
+for edge in edges:
+    first = conn_map[edge[0]]
+    second = conn_map[edge[1]]
+    
+    if (n:=len(first.union(second))) > max:
+        max = n
+        
+print(max * (max-1) // 2)
+    
+
+
+
+
